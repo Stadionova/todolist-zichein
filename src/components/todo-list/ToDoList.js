bd0b2975fcf71f58984ff05cbb8917659ba356ac
Stadionova <726383@mail.ru>
2019-10-17 14:30:30 +0300

    created todolist project files

---
+++ src/components/todo-list/ToDoList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './ToDoList.css';
+
+class ToDoList extends Component {
+
+    constructor(props) {
+
+        super();
+
+        this.state = {
+
+        };
+
+    }
+
+    render() {
+        return (
+            <div className='todolist'>
+                todolist will be here
+            </div>
+        );
+    }
+}
