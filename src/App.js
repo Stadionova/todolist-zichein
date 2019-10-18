2cd0a52f78ec230c6fd57723d9161f2cd89b73bf
Stadionova <726383@mail.ru>
2019-10-18 12:00:48 +0300

    corrected, finished version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import logo from './logo.png';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+import ToDoList from "./components/todo-list/ToDoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+      <ToDoList />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ToDoList from './components/todo-list/ToDoList';
 
 ReactDOM.render(
     <div>
         <App />
-
-        <ToDoList />
-
     </div>, document.getElementById('root'));
 
 serviceWorker.unregister();
